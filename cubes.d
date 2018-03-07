77a79ffbcb6a2eaecacbbc3a019b75eccbbedb19
KurtWimer <33082777+KurtWimer@users.noreply.github.com>
2018-03-06 16:05:21 -0800

    bug-fixes
    Integraded and fixed bugs in cubes.d

--- cubes.d
+++ cubes.d
@@ -1,7 +1,9 @@
 import core.vararg; //used for functions with unknown number of parameters
+import std.stdio;
+import std.range;
 
-
-class Cubes : RandomAccesInfinite!int{
+//temporary delete or copy : RandomAccesInfinite!int
+class Cubes {
 	private:
 		size_t first;
 
@@ -17,27 +19,36 @@ class Cubes : RandomAccesInfinite!int{
 
 	/*return the cube at index location*/
 	int opIndex(size_t index){
-		return (cast(int)index+first)*(cast(int)index+first)*cast(int)index+first);
+		int ind = cast(int)index;
+		return (ind + first)*(ind + first)*(ind +first);
 	}
 
-	int[] opIndex(size_t index,...){
-		/*implement*/
-	}
+	/*int[] opIndex(size_t index,...){
+		implement
+	}*/
 
 	/*Return a copy of the current Cube list*/ 
 	@property Cubes save() const{
 		return new Cubes(first);};
 
-	int[]  opSlice(size_t i, size_t j){
-		/*implement*/
+	@property int[] opSlice(size_t min, size_t max){
+		int[] a;
+		a.length = max - min; 
+		for(size_t i = min; i<max; ++i){
+			a[i - min] = opIndex(i); 
+		}
+	return a; 
 	}
 }
 
 void main()
 {
 	Cubes cubelist = new Cubes();
-	if (isRandomAccessRange(Cubes)) writeln("Cubes is a Random Access Range\n");
-	writeln("Cubes between 40-50 are:\n");
-	foreach(i; cubelist[40..50]){writeln("%d\n" cubelist[i]);}
+	if (isRandomAccessRange!Cubes) writeln("Cubes is a Random Access Range!\n");
+	else writeln("Housten we have an issue\n");
+	if (isRandomAccessRange!Cubes) writeln("Cubes has Slicing!\n");
+	else writeln("No cake for you.\n");
+	writeln("Cubes between 3-10 are:\n");
+	foreach(i; cubelist[3..10]){writeln(i);}
 }
 
