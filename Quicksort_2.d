701b8ca10ea022d700ca8e54406e5381d14c8e3e
m-cai <36802530+m-cai@users.noreply.github.com>
2018-03-10 18:18:37 -0800

    Update Quicksort_2.d
    Added comments

--- Quicksort_2.d
+++ Quicksort_2.d
@@ -4,10 +4,20 @@ import std.datetime.stopwatch;
 import std.algorithm: filter;
 import std.array: array;
 
+/**
+*Purpose: Sorts a given array of any length using the quick sort algorithm
+*Effeciency: O(n log n)
+*Params: arr = array to be sorted
+*Return: int[]
+*/
 int[] quicksort(int[] arr)
 {
+	//Base case
 	if(arr.length == 0)
 		return [];
+	
+	//Filter elements < arr[0] and concatenate them in front of arr[0]
+	//Filter elements >= arr[0] and concatenate them behind arr[0]
 	return quicksort(arr.filter!(a => a < arr[0]).array) 
 		~ arr[0] ~ quicksort(arr[1..$].filter!(a => a >= arr[0]).array); 
 }
